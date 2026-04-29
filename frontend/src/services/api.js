da769acdefbcb9f8003981d794be95e9fb6a7de6
Aparna Shegade <01fe23bci105@kletech.ac.in>
2026-04-29 23:53:33 +0530

    switched backend to AWS EC2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://legal-tech-backend-5klx.onrender.com"
+  baseURL: "http://13.60.196.241:10000"
 });
 
 export default API;
